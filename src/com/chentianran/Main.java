2d388dada684ab3b064dbc4840997a05f8f3cf11
Tianran Chen <tristan20177@gmail.com>
2017-02-13 20:08:11 -0800

    add files

--- src/com/chentianran/Main.java
+++ src/com/chentianran/Main.java
@@ -1,36 +1,40 @@
 package com.chentianran;
 
+import com.tree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Tianran on 1/8/2017.
  */
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello");
-		int[] arr = new int[]{4, 6, 3, 7};
-		String[] input = new String[] {"eat", "tea", "tan", "ate", "nat", "bat"};
-		int[] temp = new int[] {3, 2, 1, 0, 4};
-
-
-		//System.out.println(res);
-		TreeNodeX n1 = new TreeNodeX(5);
-		TreeNodeX n2 = new TreeNodeX(3);
-		TreeNodeX n3 = new TreeNodeX(8);
-		TreeNodeX n4 = new TreeNodeX(1);
-		TreeNodeX n5 = new TreeNodeX(4);
-		TreeNodeX n6 = new TreeNodeX(7);
-		TreeNodeX n7 = new TreeNodeX(9);
-
+		TreeNode n1 = new TreeNode(5);
+		TreeNode n2 = new TreeNode(3);
+		TreeNode n3 = new TreeNode(7);
+		TreeNode n4 = new TreeNode(1);
 		n1.left = n2;
 		n1.right = n3;
 		n2.left = n4;
-		n2.right = n5;
-		n3.left = n6;
-		n3.right = n7;
 
-
-		TestOne sol = new TestOne();
-		int num = sol.count(n1, 1, 4);
-		System.out.println(num);
-
+		List<Integer> res = getValue(n1);
+		System.out.println(res);
 	}
+
+	private static List<Integer> getValue(TreeNode root) {
+		List<Integer> result = new ArrayList<>();
+		helper(root, result);
+		return result;
+	}
+
+	private static void helper(TreeNode root, List<Integer> result) {
+		if(root == null || result.size() >= 2) {
+			return;
+		}
+		helper(root.right, result);
+		result.add(root.val);
+		helper(root.left, result);
+	}
+
 }
