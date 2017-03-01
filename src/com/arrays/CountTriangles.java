ab6a502f481ce165d304e86f906c02b4739ec993
Tianran Chen <tristan20177@gmail.com>
2017-03-01 00:53:31 -0800

    Add files

---
+++ src/com/arrays/CountTriangles.java
@@ -0,0 +1,31 @@
+package com.arrays;
+
+/**
+ * Created by Tianran on 2/28/2017.
+ */
+public class CountTriangles {
+	public int count(int[] nums) {
+		if(nums == null || nums.length <= 2) {
+			return 0;
+		}
+		int result = 0;
+		for(int i = 0; i < nums.length - 2; i++) {
+			int j = i + 1;
+			int k = j + 1;
+			for(; j < nums.length - 1; j++) {
+				while(k < nums.length && nums[i] + nums[j] > nums[k]) {
+					k++;
+				}
+				result += k - j - 1;
+			}
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		int[] nums = new int[] {5, 6, 8, 10, 12};
+		CountTriangles sol = new CountTriangles();
+		int res = sol.count(nums);
+		System.out.println(res);
+	}
+}
